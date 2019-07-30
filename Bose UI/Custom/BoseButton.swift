42765cdbd3d03d0a1679f5fab3a7378a9cc2f9ef
Eric Dolecki <eric_dolecki@bose.com>
2019-07-30 14:03:52 -0400

    Initial committal.

---
+++ Bose UI/Custom/BoseButton.swift
@@ -0,0 +1,142 @@
+//
+//  BoseButton.swift
+//  Bose UI
+//
+//  Created by Eric Dolecki on 7/29/19.
+//  Copyright © 2019 Eric Dolecki. All rights reserved.
+//
+
+import UIKit
+
+enum Type {
+    case Dark, Light, Primary, PrimaryDestructive, Secondary, SecondaryDestructive, Facebook, Amazon
+}
+
+class BoseButton: UIButton {
+
+    var myType: Type = .Dark
+    let lightGrey = UIColor(hex: "#edededff")
+    
+    // The height is controlled by the Class. Just supply the width.
+    // Position it after creating it by using origin x and/or y or center.
+    init(width: CGFloat, title: String, type: Type)
+    {
+        let frame = CGRect(x: 0, y: 0, width: width, height: 40)
+        super.init(frame: frame)
+        self.layer.cornerRadius = frame.height / 2
+        self.layer.masksToBounds = true
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = UIFont(name: "GothamSSm-Bold", size: 13.0)
+        myType = type
+        conditionUI()
+    }
+    
+    func conditionUI()
+    {
+        if myType == .Dark {
+            self.setTitleColor(UIColor.white, for: .normal)
+            self.backgroundColor = UIColor.black
+        } else if myType == .Light || myType == .Secondary {
+            self.setTitleColor(UIColor.black, for: .normal)
+            self.backgroundColor = UIColor(hex: "#edededff")
+        } else if myType == .Primary {
+            self.setTitleColor(UIColor.white, for: .normal)
+            self.backgroundColor = UIColor.black
+        } else if myType == .PrimaryDestructive {
+            self.setTitleColor(UIColor.white, for: .normal)
+            self.backgroundColor = UIColor(hex: "#b51a0eff")
+        } else if myType == .SecondaryDestructive {
+            self.setTitleColor(UIColor(hex: "#b51a0eff"), for: .normal)
+            self.backgroundColor = UIColor(hex: "#edededff")
+        } else if myType == .Facebook {
+            self.setTitleColor(UIColor.white, for: .normal)
+            self.backgroundColor = UIColor(hex: "#3b5998ff")
+            self.setImage(UIImage(named: "fb"), for: .normal)
+        } else if myType == .Amazon {
+            self.setTitleColor(UIColor.black, for: .normal)
+            self.backgroundColor = UIColor(hex: "#FF9900ff")
+            self.setImage(UIImage(named: "amazon"), for: .normal)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if imageView != nil {
+            imageEdgeInsets = UIEdgeInsets(top: 5, left: 20, bottom: 5, right: (bounds.width - 45))
+            titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: (imageView?.frame.width)!)
+        }
+    }
+    
+    override open var isHighlighted: Bool
+    {
+        didSet {
+            
+            // Scaling on press and release.
+            if isHighlighted {
+                UIView.animate(withDuration: 0.2) {
+                    self.transform = CGAffineTransform(scaleX: 0.98,y: 0.98)
+                }
+            } else {
+                UIView.animate(withDuration: 0.4) {
+                    self.transform = CGAffineTransform.identity
+                }
+            }
+            
+            if myType == .Dark {
+                
+                backgroundColor = isHighlighted ? UIColor.darkGray : UIColor.black
+                
+            } else if myType == .Light || myType == .Secondary {
+                
+                backgroundColor = isHighlighted ? lightGrey : UIColor(hex: "#edededff")
+                if isHighlighted {
+                    setTitleColor(UIColor.darkGray, for: .normal)
+                } else {
+                    setTitleColor(UIColor.black, for: .normal)
+                }
+                
+            } else if myType == .Primary {
+                
+                backgroundColor = isHighlighted ? UIColor.darkGray : UIColor.black
+                
+            } else if myType == .PrimaryDestructive {
+                
+                backgroundColor = isHighlighted ? UIColor(hex: "#b51a0eff")?.withAlphaComponent(0.7) : UIColor(hex: "#b51a0eff")
+                
+            } else if myType == .SecondaryDestructive {
+                
+                if isHighlighted {
+                    setTitleColor(UIColor(hex: "#b51a0eff")?.withAlphaComponent(0.7), for: .normal)
+                } else {
+                    setTitleColor(UIColor(hex: "#b51a0eff"), for: .normal)
+                }
+                
+            } else if myType == .Facebook {
+                
+                backgroundColor = isHighlighted ? UIColor(hex: "#4a70c2ff") : UIColor(hex: "#3b5998ff")
+                if isHighlighted {
+                    setTitleColor(UIColor(hex: "#DDDDDDff"), for: .highlighted)
+                } else {
+                    setTitleColor(UIColor.white, for: .normal)
+                }
+                let tintedImage = imageView?.image?.withRenderingMode(.alwaysTemplate)
+                self.setImage(tintedImage, for: .highlighted)
+                self.tintColor = .white
+                
+            } else if myType == .Amazon {
+                
+                backgroundColor = isHighlighted ? UIColor(hex: "#fab650ff") : UIColor(hex: "#FF9900ff")
+                if isHighlighted {
+                    setTitleColor(UIColor(hex: "#555555ff"), for: .highlighted)
+                } else {
+                    setTitleColor(UIColor.black, for: .normal)
+                }
+                let tintedImage = imageView?.image?.withRenderingMode(.alwaysTemplate)
+                self.setImage(tintedImage, for: .highlighted)
+                self.tintColor = .black
+            }
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
---
+++ Bose UI/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Bose UI
+//
+//  Created by Eric Dolecki on 7/29/19.
+//  Copyright © 2019 Eric Dolecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var myScrollView: UIScrollView!
+    var darkButton:     BoseButton!
+    var lightButton:    BoseButton!
+    var primaryButton:  BoseButton!
+    var primDest:       BoseButton!
+    var secondary:      BoseButton!
+    var secondDest:     BoseButton!
+    var facebookButton: BoseButton!
+    var amazonButton:   BoseButton!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        myScrollView = UIScrollView(frame: CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height - 400))
+        myScrollView.layer.borderColor = UIColor.lightGray.cgColor
+        myScrollView.layer.borderWidth = 1.0
+        
+        darkButton = BoseButton(width: self.view.frame.width - 80, title: "Dark Button", type: .Dark)
+        darkButton.center = CGPoint(x: self.view.frame.midX, y: 30)
+        darkButton.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        lightButton = BoseButton(width: self.view.frame.width - 80, title: "Light Button", type: .Light)
+        lightButton.center = CGPoint(x: self.view.frame.midX, y: darkButton.frame.origin.y + darkButton.frame.height + 30)
+        lightButton.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        primaryButton = BoseButton(width: self.view.frame.width - 80, title: "Primary Button", type: .Primary)
+        primaryButton.center = CGPoint(x: self.view.frame.midX, y: lightButton.frame.origin.y + lightButton.frame.height + 30)
+        primaryButton.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        primDest = BoseButton(width: self.view.frame.width - 80, title: "Primary Destructive", type: .PrimaryDestructive)
+        primDest.center = CGPoint(x: self.view.frame.midX, y: primaryButton.frame.origin.y + primaryButton.frame.height + 30)
+        primDest.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        secondary = BoseButton(width: self.view.frame.width - 80, title: "Secondary", type: .Secondary)
+        secondary.center = CGPoint(x: self.view.frame.midX, y: primDest.frame.origin.y + primDest.frame.height + 30)
+        secondary.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        secondDest = BoseButton(width: self.view.frame.width - 80, title: "Secondary Destructive", type: .SecondaryDestructive)
+        secondDest.center = CGPoint(x: self.view.frame.midX, y: secondary.frame.origin.y + secondary.frame.height + 30)
+        secondDest.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        facebookButton = BoseButton(width: self.view.frame.width - 80, title: "Facebook", type: .Facebook)
+        facebookButton.center = CGPoint(x: self.view.frame.midX, y: secondDest.frame.origin.y + secondDest.frame.height + 30)
+        facebookButton.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        amazonButton = BoseButton(width: self.view.frame.width - 80, title: "Facebook", type: .Amazon)
+        amazonButton.center = CGPoint(x: self.view.frame.midX, y: facebookButton.frame.origin.y + facebookButton.frame.height + 30)
+        amazonButton.addTarget(self, action: #selector(buttonReleased(sender:)), for: .touchUpInside)
+        
+        myScrollView.addSubview(darkButton)
+        myScrollView.addSubview(lightButton)
+        myScrollView.addSubview(primaryButton)
+        myScrollView.addSubview(primDest)
+        myScrollView.addSubview(secondary)
+        myScrollView.addSubview(secondDest)
+        myScrollView.addSubview(facebookButton)
+        myScrollView.addSubview(amazonButton)
+        self.view.addSubview(myScrollView)
+        
+        myScrollView.contentSize = CGSize(width: myScrollView.frame.width, height: amazonButton.frame.origin.y + amazonButton.frame.height + 30)
+    }
+    
+    @objc func buttonReleased(sender: BoseButton) {
+        print("release.")
+    }
+}
+

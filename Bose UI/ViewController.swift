f6a6eefbeeee2119febeedc4e66580c5caf9e650
Eric Dolecki <eric_dolecki@bose.com>
2019-07-31 09:31:23 -0400

    Reconfigured UIColor extension to have multiple initializers (one of which assumes you supply alpha value), and theme colors. Also showing theme colors in the scroll view for preview.

--- Bose UI/ViewController.swift
+++ Bose UI/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(brightnessChanged), name: UIScreen.brightnessDidChangeNotification, object: nil)
         print("Screen brightness: \(UIScreen.main.brightness)")
         
-        myScrollView = UIScrollView(frame: CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height - 400))
+        myScrollView = UIScrollView(frame: CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height - 350))
         myScrollView.layer.borderColor = UIColor.lightGray.cgColor
         myScrollView.layer.borderWidth = 1.0
         
@@ -74,7 +74,53 @@ class ViewController: UIViewController {
         myScrollView.addSubview(amazonButton)
         self.view.addSubview(myScrollView)
         
-        myScrollView.contentSize = CGSize(width: myScrollView.frame.width, height: amazonButton.frame.origin.y + amazonButton.frame.height + 30)
+        // Place colored views to show off the theme colors.
+        
+        var positionY = amazonButton.frame.origin.y + amazonButton.frame.height + 20
+        let dictionary: [String: UIColor] = [
+            "Fern" : UIColor.FlatColor.Green.Fern,
+            "Mountain Meadow" : UIColor.FlatColor.Green.MountainMeadow,
+            "Chateau Green" : UIColor.FlatColor.Green.ChateauGreen,
+            "Persian Green" : UIColor.FlatColor.Green.PersianGreen,
+            "Almond Frost" : UIColor.FlatColor.Gray.AlmondFrost,
+            "White Smoke" : UIColor.FlatColor.Gray.WhiteSmoke,
+            "Iron Gray" : UIColor.FlatColor.Gray.IronGray,
+            "Iron" : UIColor.FlatColor.Gray.Iron,
+            "Picton Blue" : UIColor.FlatColor.Blue.PictonBlue,
+            "Mariner" : UIColor.FlatColor.Blue.Mariner,
+            "Curious Blue" : UIColor.FlatColor.Blue.CuriousBlue,
+            "Denim" : UIColor.FlatColor.Blue.Denim,
+            "Wisternia" : UIColor.FlatColor.Violet.Wisteria,
+            "Blue Gem" : UIColor.FlatColor.Violet.BlueGem,
+            "Chambray" : UIColor.FlatColor.Blue.Chambray,
+            "Blue Whale" : UIColor.FlatColor.Blue.BlueWhale,
+            "Energy" : UIColor.FlatColor.Yellow.Energy,
+            "Turbo" : UIColor.FlatColor.Yellow.Turbo,
+            "Neon Carrot" : UIColor.FlatColor.Orange.NeonCarrot,
+            "Sun" : UIColor.FlatColor.Orange.Sun,
+            "Terra Cotta" : UIColor.FlatColor.Red.TerraCotta,
+            "Valencia" : UIColor.FlatColor.Red.Valencia,
+            "Cinnabar" : UIColor.FlatColor.Red.Cinnabar,
+            "Well Read" : UIColor.FlatColor.Red.WellRead
+        ]
+        
+        for (key, value) in dictionary {
+            //print("\(key): \(value)")
+            let view = UIView(frame: CGRect(x: 40, y: positionY, width: amazonButton.frame.width, height: 30))
+            let label = UILabel(frame: CGRect(x: 10, y: 5, width: view.frame.width - 10, height: 20))
+            label.textColor = UIColor.white
+            if key == "Turbo" || key == "Energy" || key == "White Smoke" || key == "Iron" {
+                label.textColor = UIColor.darkGray
+            }
+            label.font = UIFont(name: "GothamSSm-Bold", size: 9.0)
+            label.text = key.uppercased()
+            view.addSubview(label)
+            view.backgroundColor = value
+            myScrollView.addSubview(view)
+            positionY += 31
+        }
+        
+        myScrollView.contentSize = CGSize(width: myScrollView.frame.width, height: positionY + 10)
     }
     
     // The range is 0.0 - 1.0
